a81fe42213cfc8e769d07c9b66ebe4bf0153fa45
Angel Villanueva <avillanueva@sinapse.es>
2013-07-04 21:06:27 +0200

    Main Game State

---
+++ MyFramework/MainGameState.h
@@ -0,0 +1,13 @@
+//
+//  MainGameState.h
+//  MyFramework
+//
+//  Created by Angel Villanueva Pérez on 04/07/13.
+//  Copyright (c) 2013 Angel Villanueva Pérez. All rights reserved.
+//
+
+#import "GameState.h"
+
+@interface MainGameState : GameState
+
+@end
---
+++ MyFramework/MainGameState.m
@@ -0,0 +1,60 @@
+//
+//  MainGameState.m
+//  MyFramework
+//
+//  Created by Angel Villanueva Pérez on 04/07/13.
+//  Copyright (c) 2013 Angel Villanueva Pérez. All rights reserved.
+//
+
+#import "MainGameState.h"
+
+@implementation MainGameState
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (id) initWithFrame:(CGRect)frame andManager:(GameStateManager *)pManager {
+    
+    if (self = [super initWithFrame:frame andManager:pManager]) {
+        
+        NSLog(@"Main Game State initialized");
+        
+    }
+    
+    return self;
+    
+}
+
+- (void) touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
+    
+    UITouch *touch = [touches anyObject];
+    NSUInteger numTaps = [touch tapCount];
+    
+    // todo: implement touch code
+    
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+*/
+- (void)drawRect:(CGRect)rect
+{
+    CGContextRef g = UIGraphicsGetCurrentContext();
+    
+    // fill background with gray
+    CGContextSetFillColorWithColor(g, [UIColor grayColor].CGColor);
+    CGContextFillRect(g, CGRectMake(0, 0, self.frame.size.width, self.frame.size.height));
+    // draw text in black
+    CGContextSetFillColorWithColor(g, [UIColor blackColor].CGColor);
+    [@"Rendered from Main Game State" drawAtPoint:CGPointMake(10.0, 20.0) withFont:[UIFont systemFontOfSize:[UIFont systemFontSize]]];
+}
+
+
+@end
